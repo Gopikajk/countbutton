76d7d86fb1512341c82ce194fc07906a523ab87c
Gopika JK <72131148+Gopikajk@users.noreply.github.com>
2020-10-11 19:30:47 +0530

    Add files via upload

---
+++ boot.js
@@ -0,0 +1,24 @@
+var add= document.getElementById("inc");
+var add=document.getElementById("dec");
+var add=document.getElementById("count");
+
+
+var integer=0;
+function increment()
+{
+   integer=integer+1;
+  count.innerHTML=integer;
+
+}
+function reset()
+{
+   integer=0;
+  count.innerHTML=integer;
+
+}
+function decrement()
+{
+   integer=integer-1;
+  count.innerHTML=integer;
+
+}
